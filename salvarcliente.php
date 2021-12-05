4d33c8160614b6d53d6895a9e51bd87fb29cb3ca
GustavoCosta197 <86078070+GustavoCosta197@users.noreply.github.com>
2021-12-05 20:30:06 -0300

    Update salvarcliente.php

--- salvarcliente.php
+++ salvarcliente.php
@@ -5,7 +5,36 @@ $nomeCliente = $_GET["nome"];
 $cpfCliente = $_GET["cpf"];
 $foneCliente = $_GET["fone"];
 $emailCliente = $_GET["email"];
-echo $nomeCliente . "<br>";
-echo $cpfCliente . "<br>";
-echo $foneCliente . "<br>";
-echo $emailCliente . "<br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salvar Cliente</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>Nome Completo</td>
+            <td><?php echo $nomeCliente; ?></td>
+        </tr>
+        <tr>
+            <td>CPF</td>
+            <td><?php echo $cpfCliente; ?></td>
+        </tr>
+        <tr>
+            <td>Telefone</td>
+            <td><?php echo $foneCliente; ?></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><?php echo $emailCliente; ?></td>
+        </tr>
+
+    </table>
+</body>
+</html>
